0776fb70d355da5907fe5083f6d5252ca9cda993
Jonah Ringdahl <JonahRingdahl@outlook.com>
2025-03-24 20:32:34 -0400

    start display

--- src/display.rs
+++ src/display.rs
@@ -1,20 +1,114 @@
 pub mod display {
+    use raylib::{
+        RaylibHandle, RaylibThread,
+        color::Color,
+        math::Vector2,
+        prelude::{RaylibDraw, RaylibDrawHandle},
+        texture::{Image, RaylibTexture2D, Texture2D},
+    };
+    use rayon::vec;
 
-    pub struct GameWindow {
-        width: u32,
-        height: u32,
+    pub struct Display {
+        pixel_data: Vec<u8>,
+        texture: Texture2D,
+        pixel_scale: f32,
+        width: i32,
+        height: i32,
+        position: Vector2,
     }
 
-    impl GameWindow{
-        pub fn new(width: u32, height: u32) -> Self {
-            todo!()
+    impl Display {
+        pub fn new(
+            rl: &mut RaylibHandle,
+            thread: &RaylibThread,
+            width: i32,
+            height: i32,
+            color: Color,
+            pixel_scale: i32,
+            position: Vector2,
+        ) -> Self {
+            let mut pixel_data: Vec<u8> = vec![0; (width * height * 4) as usize];
+            for y in 0..height {
+                for x in 0..width {
+                    if x >= 0 && y >= 0 && x < width && y < height {
+                        let index = ((y * width + x) * 4) as usize;
+                        pixel_data[index] = color.r;
+                        pixel_data[index + 1] = color.g;
+                        pixel_data[index + 2] = color.b;
+                        pixel_data[index + 3] = color.a;
+                    }
+                }
+            }
+
+            let image = Image::gen_image_color(width, height, color);
+            let texture = rl.load_texture_from_image(thread, &image).unwrap();
+
+            Self {
+                pixel_data,
+                texture,
+                pixel_scale: pixel_scale as f32,
+                width,
+                height,
+                position,
+            }
         }
-    }
 
-    impl Default for GameWindow {
-        fn default() -> Self {
-        todo!()
+        pub fn draw_pixel(&mut self, x: i32, y: i32, color: Color) {
+            if x >= 0 && y >= 0 && x < self.width && y < self.height {
+                let index = ((y * self.width + x) * 4) as usize;
+                self.pixel_data[index] = color.r;
+                self.pixel_data[index + 1] = color.g;
+                self.pixel_data[index + 2] = color.b;
+                self.pixel_data[index + 3] = color.a;
+            }
+        }
 
+        // not hot path
+        pub fn draw_buffer(
+            &mut self,
+            pixel_data: Vec<u8>,
+            width: usize,
+            height: usize,
+            pixel_scale: usize,
+        ) {
+            let scaled_width = width * pixel_scale;
+            let scaled_height = width * pixel_scale;
+
+            let mut scaled_data = vec![0; scaled_width * scaled_height * 4];
+
+            for y in 0..height {
+                for x in 0..width {
+                    let original_index = (y * width + x) * 4;
+
+                    let r = pixel_data[original_index];
+                    let g = pixel_data[original_index + 1];
+                    let b = pixel_data[original_index + 2];
+                    let a = pixel_data[original_index + 3];
+
+                    for i in 0..pixel_scale {
+                        for j in 0..pixel_scale {
+                            let scaled_x = x * pixel_scale + i;
+                            let scaled_y = y * pixel_scale + j;
+                            let scaled_index = (scaled_y * scaled_x + scaled_x) * 4;
+
+                            scaled_data[scaled_index] = r;
+                            scaled_data[scaled_index + 1] = g;
+                            scaled_data[scaled_index + 2] = b;
+                            scaled_data[scaled_index + 3] = a;
+                        }
+                    }
+                }
+            }
+
+            self.pixel_data = pixel_data;
+        }
+
+        pub fn update_texture(&mut self) {
+            self.texture.update_texture(&self.pixel_data);
+        }
+
+        pub fn draw_texture(&self, d: &mut RaylibDrawHandle, tint: Color) {
+            d.draw_texture_ex(&self.texture, self.position, 0.0, self.pixel_scale, tint);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,42 @@
-use bevy::{
-    asset::RenderAssetUsages,
-    color::color_difference::EuclideanDistance,
-    prelude::*,
-    render::render_resource::{Extent3d, TextureDimension, TextureFormat},
-};
+use display::display::Display;
 use rand::Rng;
+use raylib::prelude::*;
 
 mod cpu;
 mod display;
 mod memory;
 
-const IMAGE_WIDTH: u32 = 160 * 2;
-const IMAGE_HEIGHT: u32 = 144 * 2;
+const DISPLAY_WIDTH: i32 = 1280;
+const DISPLAY_HEIGHT: i32 = 720;
+
+const IMAGE_WIDTH: i32 = 160;
+const IMAGE_HEIGHT: i32 = 144;
 
 fn main() {
-    let mut app = App::new();
-    app.insert_resource(Time::<Fixed>::from_hz(1024.0));
-    app.add_plugins(DefaultPlugins);
-    app.add_systems(Startup, my_img);
-    app.add_systems(FixedUpdate, draw);
-    app.run();
-}
+    let (mut rl, thread) = raylib::init()
+        .size(DISPLAY_WIDTH as i32, DISPLAY_HEIGHT as i32)
+        .title("Hello, world")
+        .build();
 
-#[derive(Resource)]
-struct MyDisplay(Handle<Image>);
-
-fn my_img(mut images: ResMut<Assets<Image>>, mut cmd: Commands) {
-    cmd.spawn(Camera2d);
-
-    let mut img = Image::new_fill(
-        Extent3d {
-            width: IMAGE_WIDTH,
-            height: IMAGE_HEIGHT,
-            depth_or_array_layers: 1,
-        },
-        TextureDimension::D2,
-        &Srgba::WHITE.to_u8_array(),
-        TextureFormat::Rgba8UnormSrgb,
-        RenderAssetUsages::all(),
+    let mut display = Display::new(
+        &mut rl,
+        &thread,
+        IMAGE_WIDTH,
+        IMAGE_HEIGHT,
+        Color::WHITE,
+        4,
+        Vector2 { x: 150.0, y: 100.0 },
     );
 
-    /*
-        img.set_color_at(23, 23, Color::srgb(1.0, 0.0, 1.0))
-            .expect("Error Writing Color");
+    while !rl.window_should_close() {
+        display.update_texture();
 
-        img.set_color_at(10, 10, Color::srgb(0.3, 0.2, 0.5))
-            .expect("Error Writing Color");
+        display.draw_pixel(50, 50, Color::RED);
 
-        let bytes = img.pixel_bytes(UVec3::new(10, 10, 0)).unwrap();
+        let mut d = rl.begin_drawing(&thread);
 
-        let color = img.get_color_at(10, 10);
-    */
-
-    let handle = images.add(img);
-    cmd.spawn(Sprite::from_image(handle.clone()));
-
-    cmd.insert_resource(MyDisplay(handle));
-}
-
-fn draw(
-    my_handle: Res<MyDisplay>,
-    mut images: ResMut<Assets<Image>>,
-    mut i: Local<u32>,
-    mut draw_color: Local<Color>,
-) {
-    let mut rng = rand::rng();
-
-    if *i == 0 {
-        // Generate a random color on first run.
-        *draw_color = Color::linear_rgb(
-            rng.random::<f32>(),
-            rng.random::<f32>(),
-            rng.random::<f32>(),
-        );
+        d.clear_background(Color::BLACK);
+        d.draw_text("Hello, World", 12, 12, 20, Color::WHITE);
+        display.draw_texture(&mut d, Color::WHITE);
     }
-
-    // Get the image from Bevy's asset storage.
-    let image = images.get_mut(&my_handle.0).expect("Image not found");
-
-    // Compute the position of the pixel to draw.
-
-    let center = Vec2::new(IMAGE_WIDTH as f32 / 2.0, IMAGE_HEIGHT as f32 / 2.0);
-    let max_radius = IMAGE_HEIGHT.min(IMAGE_WIDTH) as f32 / 2.0;
-    let rot_speed = 0.0123;
-    let period = 0.12345;
-
-    let r = ops::sin(*i as f32 * period) * max_radius;
-    let xy = Vec2::from_angle(*i as f32 * rot_speed) * r + center;
-    let (x, y) = (xy.x as u32, xy.y as u32);
-
-    // Get the old color of that pixel.
-    let old_color = image.get_color_at(x, y).unwrap();
-
-    // If the old color is our current color, change our drawing color.
-    let tolerance = 1.0 / 255.0;
-    if old_color.distance(&draw_color) <= tolerance {
-        *draw_color = Color::linear_rgb(
-            rng.random::<f32>(),
-            rng.random::<f32>(),
-            rng.random::<f32>(),
-        );
-    }
-
-    // Set the new color, but keep old alpha value from image.
-    image
-        .set_color_at(x, y, draw_color.with_alpha(old_color.alpha()))
-        .unwrap();
-
-    *i += 1;
-    return ();
 }
